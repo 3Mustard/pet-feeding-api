b74bf8790f6e5855c99bac8d82cc24e63fa14766
Matt <m23.cullen@gmail.com>
2020-02-26 09:58:42 -0500

    seeded database to test models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+akira = Pet.new(name: "Akira", gender: "male", last_fed: "03-23-2020", next_feeding: "03-24-2020")
+note = Note.new(title: "Why Akira is a good cat", body: "he is very soft and nice")
+
+akira.notes << note
+akira.save
