b3526746dc890cc47e48d17413da26efc99b2bba
Matt <m23.cullen@gmail.com>
2020-02-26 21:35:13 -0500

    set up notes controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,3 +1,28 @@
 class Api::V1::NotesController < ApplicationController
 
+  def index
+    pet = Pet.find_by(id: params[:pet_id])
+
+    render json: pet.notes
+  end
+
+  def show
+    note = Note.find_by(id: params[:id])
+
+    render json: note
+  end
+
+  def create
+    pet = Pet.find_by(id: params[:pet_id])
+    pet.notes.new(note_params)
+    pet.save
+
+    render json: pet
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
 end
